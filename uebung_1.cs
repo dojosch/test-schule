a3f80118b96537b2bbc53f756f43b507814fbbac
Dominik Schratl <hi@dominik-schratl.at>
2023-05-08 08:46:38 +0200

    init

---
+++ uebung_1.cs
@@ -0,0 +1,6 @@
+double[] waterAmount = { 2.5 , 2.5 , 0.5 , 2.5 , 1.5 , 2.5 , 0.5 , 1.5 , 3.5 , 2.5 , 3.5 , 2.5 , 0.5 , 0.5 , 1.5 , 3.5 , 2.5 , 0.5 , 2.5 , 0.5 , 2.5 , 2.5 , 1.5 , 2.5 , 1.5 , 1.5 , 3.5 , 1.5 , 1.5 , 1.5 , 0.5 };
+int waterOutOfRangeCount = 0;
+
+// YOUR CODE HERE
+
+Console.WriteLine($"Du hast an {waterOutOfRangeCount} Tagen zu wenig oder zu viel getrunken.");
